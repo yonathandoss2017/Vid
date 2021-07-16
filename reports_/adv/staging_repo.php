3990f31e4b005884427a3600ee0f9ee391727b2b
Pablo Magarinos <pablo.magarinos@vidoomy.com>
2021-07-16 12:54:43 +0300

    feature/pv-202-hierarchy_reporting_logic_countries
    Removed extra parenthesis when user is admin

--- reports_/adv/staging_repo.php
+++ reports_/adv/staging_repo.php
@@ -240,7 +240,7 @@
         }
     }
 
-    $PubManFilter .= ")";
+    $PubManFilter .= $PubManFilter === "" ? $PubManFilter : ")";
 
 	header('Access-Control-Allow-Origin: *');
 	header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
