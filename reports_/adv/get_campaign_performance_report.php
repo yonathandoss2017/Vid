5a0acbc230b725b8435afa65f746e18fb3241b7b
mohamadElmordaa <mohamad.elmurdaa@gmail.com>
2021-08-30 15:13:32 +0300

    PV-90 calculating the actual and consumed budgets depending on the hour the email is sent

--- reports_/adv/get_campaign_performance_report.php
+++ reports_/adv/get_campaign_performance_report.php
@@ -14,22 +14,56 @@ require('../../db.php');
 $db = new SQL($dbhost, $dbAdvName, $dbuser, $dbpass);
 
 $dt = new DateTime();
-$dt->setTimezone(new DateTimeZone('Europe/Berlin'));
-$dateTime = $dt->format('H') <= 12 ? date("Y-m-d H:i:s\n", strtotime("yesterday 15:00")) : date("Y-m-d H:i:s\n", strtotime("today 12:00"));
-$currentDateTime = $dt->format('Y-m-d H:i:s');
+$dt->setTimezone(new DateTimeZone('UTC'));
 
-$sql = "SELECT r.idCampaing AS id, total.total_revenue, SUM(r.CompleteV)/SUM(r.Impressions)*100 AS VTR, SUM(r.VImpressions)/SUM(r.Impressions)*100 AS Viewability, SUM(r.Clicks)/SUM(r.Impressions)*100 AS CTR
-        FROM reports r,
-        (
-          SELECT idCampaing, SUM(revenue) AS total_revenue
-              FROM reports
-              GROUP BY idCampaing
-        ) total
-        WHERE r.idCampaing = total.idCampaing
-        AND TIMESTAMP(r.date, SEC_TO_TIME(r.hour*3600)) BETWEEN '$dateTime' AND '$currentDateTime'
+if ($dt->format('H') <= 9){
+
+    $dateTimeConsumedBudgetFrom = date("Y-m-d H:i:s", strtotime("yesterday -1 day 00:00:00"));
+    $dateTimeConsumedBudgetTo = date("Y-m-d H:i:s", strtotime("yesterday -1 day 23:59:59"));
+    $dateTimeFrom = date("Y-m-d H:i:s", strtotime("yesterday 00:00:00"));
+    $dateTimeTo = date("Y-m-d H:i:s", strtotime("yesterday 23:59:59"));
+
+    $checkConsumedBudgetData = "SELECT idCampaing AS id FROM reports
+     WHERE TIMESTAMP(date, SEC_TO_TIME(hour*3600)) BETWEEN '$dateTimeConsumedBudgetFrom' AND '$dateTimeConsumedBudgetTo' ";
+
+    if (0 < $db->num_rows($db->query($checkConsumedBudgetData))) {
+
+        $sql = "SELECT r.idCampaing AS id, consumedBugdet.consumed_impressions, consumedBugdet.consumed_viewability, consumedBugdet.consumed_clicks, consumedBugdet.consumed_completes, SUM(r.Impressions) AS actual_impressions, SUM(r.VImpressions) AS actual_viewability, SUM(r.CompleteV) AS actual_completes , SUM(r.Clicks) AS actual_clicks
+            FROM reports r,
+            (
+              SELECT idCampaing, SUM(Impressions) AS consumed_impressions, SUM(VImpressions) AS consumed_viewability, SUM(Clicks) AS consumed_clicks, SUM(CompleteV) AS consumed_completes
+                  FROM reports
+                  WHERE TIMESTAMP(date, SEC_TO_TIME(hour*3600)) BETWEEN '$dateTimeConsumedBudgetFrom' AND '$dateTimeConsumedBudgetTo'
+                  GROUP BY idCampaing
+            ) consumedBugdet
+            WHERE r.idCampaing = consumedBugdet.idCampaing
+            AND TIMESTAMP(r.date, SEC_TO_TIME(r.hour*3600)) BETWEEN '$dateTimeFrom' AND '$dateTimeTo'
+            GROUP BY r.idCampaing
+            ORDER BY r.idCampaing ASC;
+        ";
+    }
+    else {
+        $sql = "SELECT r.idCampaing AS id, SUM(r.Impressions) AS actual_impressions, SUM(r.VImpressions) AS actual_viewability, SUM(r.CompleteV) AS actual_completes , SUM(r.Clicks) AS actual_clicks
+            FROM reports r
+            WHERE r.idCampaing
+            AND TIMESTAMP(r.date, SEC_TO_TIME(r.hour*3600)) BETWEEN '$dateTimeFrom' AND '$dateTimeTo'
+            GROUP BY r.idCampaing
+            ORDER BY r.idCampaing ASC;
+        ";
+    }
+}
+else{
+    $dateTimeFrom = date("Y-m-d H:i:s", strtotime("today 00:00:00"));
+    $dateTimeTo = date("Y-m-d H:i:s", strtotime("today 15:00:00"));
+
+    $sql = "SELECT r.idCampaing AS id, SUM(r.Impressions) AS actual_impressions, SUM(r.VImpressions) AS actual_viewability, SUM(r.CompleteV) AS actual_completes , SUM(r.Clicks) AS actual_clicks
+        FROM reports r
+        WHERE r.idCampaing
+        AND TIMESTAMP(r.date, SEC_TO_TIME(r.hour*3600)) BETWEEN '$dateTimeFrom' AND '$dateTimeTo'
         GROUP BY r.idCampaing
         ORDER BY r.idCampaing ASC;
     ";
+}
 
 $query = $db->query($sql);
 
@@ -38,10 +72,14 @@ if(0 < $db->num_rows($query)) {
     while($campaign = $db->fetch_array($query)) {
         $campaigns[] = [
             'id' => $campaign['id'],
-            'VTR' => $campaign['VTR'],
-            'Viewability' => $campaign['Viewability'],
-            'CTR' => $campaign['CTR'],
-            'total_revenue' => $campaign['total_revenue'],
+            'consumed_impressions' => $campaign['consumed_impressions'],
+            'consumed_viewability' => $campaign['consumed_viewability'],
+            'consumed_clicks' => $campaign['consumed_clicks'],
+            'consumed_completes' => $campaign['consumed_completes'],
+            'actual_impressions' => $campaign['actual_impressions'],
+            'actual_viewability' => $campaign['actual_viewability'],
+            'actual_completes' => $campaign['actual_completes'],
+            'actual_clicks' => $campaign['actual_clicks'],
         ];
     }
 }
