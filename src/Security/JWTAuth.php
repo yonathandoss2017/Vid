fd0e484bb7e824730d4d042c99b44e5682d5749b
Vidoomy Media <finance@vidoomy.com>
2022-01-11 18:15:44 +0000

    Check defined CONST and fix for Apache headers

--- src/Security/JWTAuth.php
+++ src/Security/JWTAuth.php
@@ -78,7 +78,23 @@ class JWTAuth
     }
 
     public static function getAuthorizationHeader() {
-        return $_SERVER["Authorization"] ?? $_SERVER["HTTP_AUTHORIZATION"] ?? '';
+        //return $_SERVER["Authorization"] ?? $_SERVER["HTTP_AUTHORIZATION"] ?? '';
+   	$headers = null;
+    if (isset($_SERVER['Authorization'])) {
+        $headers = trim($_SERVER["Authorization"]);
+    }
+    else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
+        $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
+    } elseif (function_exists('apache_request_headers')) {
+        $requestHeaders = apache_request_headers();
+        // Server-side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
+        $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
+        //print_r($requestHeaders);
+        if (isset($requestHeaders['Authorization'])) {
+            $headers = trim($requestHeaders['Authorization']);
+        }
+    }
+    return $headers; 
     }
 
     public static function getRequestToken() {
