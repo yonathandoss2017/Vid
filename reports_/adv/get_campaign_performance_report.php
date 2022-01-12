ed4c3f7cf943fe694391479c877b7659134ee47f
mohamadElmordaa <mohamad.elmurdaa@gmail.com>
2022-01-12 16:01:27 +0200

    PV-90 KPIs Campaign performance reports queries

--- reports_/adv/get_campaign_performance_report.php
+++ reports_/adv/get_campaign_performance_report.php
@@ -16,50 +16,36 @@ $db = new SQL($dbhost, $dbAdvName, $dbuser, $dbpass);
 $dt = new DateTime();
 $dt->setTimezone(new DateTimeZone('CET'));
 
-if ($dt->format('H') < 13) {
-    $dayBeforeYesterday = date("Y-m-d", strtotime("yesterday -1 day"));
-    $yesterday = date("Y-m-d", strtotime("yesterday"));
+$today = date("Y-m-d");
+$yesterday = date("Y-m-d", strtotime("yesterday"));
 
-    $checkConsumedBudgetData = "SELECT idCampaing AS id FROM reports
-     WHERE date BETWEEN '$dayBeforeYesterday' AND '$dayBeforeYesterday' AND hour BETWEEN 0 AND 23 ";
-
-    if (0 < $db->num_rows($db->query($checkConsumedBudgetData))) {
-
-        $sql = "SELECT r.idCampaing AS id, consumedBugdet.consumed_impressions, consumedBugdet.consumed_viewability, consumedBugdet.consumed_clicks, consumedBugdet.consumed_completes, SUM(r.Impressions) AS actual_impressions, SUM(r.VImpressions) AS actual_viewability, SUM(r.CompleteV) AS actual_completes , SUM(r.Clicks) AS actual_clicks
+if ($dt->format('H') < 18) {
+        $sql = "SELECT r.idCampaing AS id, yesterday_kpi.impressions, yesterday_kpi.viewability, yesterday_kpi.clicks, yesterday_kpi.completes, SUM(r.Impressions) AS total_impressions, SUM(r.VImpressions) AS total_viewability, SUM(r.CompleteV) AS total_completes , SUM(r.Clicks) AS total_clicks
             FROM reports r,
             (
-              SELECT idCampaing, SUM(Impressions) AS consumed_impressions, SUM(VImpressions) AS consumed_viewability, SUM(Clicks) AS consumed_clicks, SUM(CompleteV) AS consumed_completes
+              SELECT idCampaing, SUM(Impressions) AS impressions, SUM(VImpressions) AS viewability, SUM(Clicks) AS clicks, SUM(CompleteV) AS completes
                   FROM reports
-                  WHERE date BETWEEN '$dayBeforeYesterday' AND '$dayBeforeYesterday' AND
-                  hour BETWEEN 0 AND 23
+                  WHERE date = '$yesterday'
                   GROUP BY idCampaing
-            ) consumedBugdet
-            WHERE r.idCampaing = consumedBugdet.idCampaing
-            AND r.date BETWEEN '$yesterday' AND '$yesterday'
-            AND r.hour BETWEEN 0 AND 23
+            ) yesterday_kpi
+            WHERE r.idCampaing = yesterday_kpi.idCampaing
+            AND r.date < '$today'
             GROUP BY r.idCampaing
             ORDER BY r.idCampaing ASC;
         ";
-    } else {
-        $sql = "SELECT r.idCampaing AS id, SUM(r.Impressions) AS actual_impressions, SUM(r.VImpressions) AS actual_viewability, SUM(r.CompleteV) AS actual_completes , SUM(r.Clicks) AS actual_clicks
-            FROM reports r
-            WHERE r.idCampaing
-            AND r.date BETWEEN '$yesterday' AND '$yesterday'
-            AND r.hour BETWEEN 0 AND 23
-            GROUP BY r.idCampaing
-            ORDER BY r.idCampaing ASC;
-        ";
-    }
 } else {
-    $today = date("Y-m-d");
-
-    $sql = "SELECT r.idCampaing AS id, SUM(r.Impressions) AS actual_impressions, SUM(r.VImpressions) AS actual_viewability, SUM(r.CompleteV) AS actual_completes , SUM(r.Clicks) AS actual_clicks
-        FROM reports r
-        WHERE r.idCampaing AND
-        r.date BETWEEN '{$today}' AND '{$today}' AND
-        r.Hour BETWEEN 0 AND 13
-        GROUP BY r.idCampaing
-        ORDER BY r.idCampaing ASC;
+    $sql = "SELECT r.idCampaing AS id, today_kpi.impressions, today_kpi.viewability, today_kpi.clicks, today_kpi.completes, SUM(r.Impressions) AS total_impressions, SUM(r.VImpressions) AS total_viewability, SUM(r.CompleteV) AS total_completes , SUM(r.Clicks) AS total_clicks
+            FROM reports r,
+            (
+              SELECT idCampaing, SUM(Impressions) AS impressions, SUM(VImpressions) AS viewability, SUM(Clicks) AS clicks, SUM(CompleteV) AS completes
+                  FROM reports
+                  WHERE date = '$today'
+                  GROUP BY idCampaing
+            ) today_kpi
+            WHERE r.idCampaing = today_kpi.idCampaing
+            AND r.date <= '$today'
+            GROUP BY r.idCampaing
+            ORDER BY r.idCampaing ASC;
     ";
 }
 
@@ -70,14 +56,14 @@ if (0 < $db->num_rows($query)) {
     while ($campaign = $db->fetch_array($query)) {
         $campaigns[] = [
             'id' => $campaign['id'],
-            'consumed_impressions' => $campaign['consumed_impressions'],
-            'consumed_viewability' => $campaign['consumed_viewability'],
-            'consumed_clicks' => $campaign['consumed_clicks'],
-            'consumed_completes' => $campaign['consumed_completes'],
-            'actual_impressions' => $campaign['actual_impressions'],
-            'actual_viewability' => $campaign['actual_viewability'],
-            'actual_completes' => $campaign['actual_completes'],
-            'actual_clicks' => $campaign['actual_clicks'],
+            'impressions' => $campaign['impressions'],
+            'viewability' => $campaign['viewability'],
+            'clicks' => $campaign['clicks'],
+            'completes' => $campaign['completes'],
+            'total_impressions' => $campaign['total_impressions'],
+            'total_viewability' => $campaign['total_viewability'],
+            'total_completes' => $campaign['total_completes'],
+            'total_clicks' => $campaign['total_clicks'],
         ];
     }
 }
