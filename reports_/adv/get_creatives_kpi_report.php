a56778359e911e2e8d11604bad8ed878ebff2368
Pablo Magarinos <pablo.magarinos@vidoomy.com>
2022-06-29 11:59:36 +0300

    feature/PNLS-15-KPI_alert_communication
    SQL query updated

--- reports_/adv/get_creatives_kpi_report.php
+++ reports_/adv/get_creatives_kpi_report.php
@@ -30,20 +30,34 @@ $toDateTimeString = $_POST['date_to'];
 $dateTo = substr($toDateTimeString, 0, 10);
 $hourTo = substr($toDateTimeString, 11, 2);
 
-$sql = sprintf(
-        "SELECT idCampaing AS campaignId, idCreativity AS creativityId,
-            SUM(Impressions) AS impressions, SUM(VImpressions) AS viewableImpressions,
-            SUM(Clicks) AS clicks, SUM(CompleteV) AS completeViews, Date AS date
-        FROM reports r
-        WHERE (Date = '%s'
-        OR (Date = '%s' AND Hour <= '%s'))
-        AND idCreativity IN (%s)
-        GROUP BY campaignId, creativityId, date
-    ",
-    $dateFrom, $dateTo, $hourTo, $creativitiesIds
-);
+$SQL = sprintf("SELECT reports.idCampaing AS campaignId,
+            demand_tag.demand_tag_id AS creativityId,
+            demand_tag.name AS creativityName,
+            reports.Date AS date,
+            SUM(reports.Impressions) AS impressions,
+            SUM(reports.VImpressions) AS viewableImpressions,
+            SUM(reports.Clicks) AS clicks,
+            SUM(reports.CompleteV) AS completeViews,
+            ROUND((SUM(reports.CompleteV) / SUM(reports.Impressions) * 100), 20) AS VTR,
+            ROUND((SUM(reports.Clicks) / SUM(reports.Impressions) * 100), 2) AS CTR,
+            ROUND((SUM(reports.VImpressions) / SUM(reports.Impressions) * 100), 2) AS viewabilityPercent
+        FROM reports
+        INNER JOIN campaign ON campaign.id = reports.idCampaing
+        LEFT JOIN demand_tag ON demand_tag.id = reports.idCreativity
+        WHERE (
+            ((reports.Date >= '%s' AND reports.Date < '%s') AND reports.Hour >= 0)
+            OR ((reports.Date > '%s' AND reports.Date <= '%s') AND reports.Hour < '%s'))
+        AND (
+          demand_tag.demand_tag_id IN (%s)
+        )
+        GROUP BY campaignId, creativityId, date",
+        $dateFrom, $dateTo,
+        $dateFrom, $dateTo, $hourTo,
+        $creativitiesIds
+    )
+;
 
-$query = $db->query($sql);
+$query = $db->query($SQL);
 
 $results = [];
 if(0 < $db->num_rows($query)) {
@@ -51,10 +65,14 @@ if(0 < $db->num_rows($query)) {
         $results[] = [
             'campaign_id' => $rows['campaignId'],
             'creativity_id' => $rows['creativityId'],
+            'creativity_name' => $rows['creativityName'],
             'impressions' => $rows['impressions'],
             'viewable_impressions' => $rows['viewableImpressions'],
             'clicks' => $rows['clicks'],
             'complete_views' => $rows['completeViews'],
+            'vtr' => $rows['VTR'],
+            'ctr' => $rows['CTR'],
+            'viewability_percent' => $rows['viewabilityPercent'],
             'date' => $rows['date'],
         ];
     }
